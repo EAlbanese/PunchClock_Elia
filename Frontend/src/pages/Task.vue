568c00d024ec35689a050c0b615fff58967ecc6d
Elia Albanese <eliaalbanese@icloud.com>
2021-11-22 13:12:13 +0100

    not inportent

--- Frontend/src/pages/Task.vue
+++ Frontend/src/pages/Task.vue
@@ -1,6 +1,25 @@
 <template>
-<q-page class="flex flex-center">
-    Create new Task
+<q-page style="padding-top: ">
+    <div class="LoginPage">
+        <h1>Create new task</h1>
+        <div style="max-width: 300px;" class="q-mx-auto">
+            <form @submit="validateAndSubmit">
+                <div v-if="errors.length">
+                    <div class="alert alert-danger" v-bind:key="index" v-for="(error, index) in errors">
+                        {{error}}
+                    </div>
+                </div>
+                <q-input v-model="firstname" type="text" class="form-control" label="First Name" />
+                <q-input v-model="lastname" type="text" class="form-control" label="Last Name" />
+                <q-input v-model="email" type="text" class="form-control" label="E-Mail" />
+                <q-input v-model="password" type="text" class="form-control" label="Password" />
+            </form>
+        </div>
+        <div style="padding-top: 10px;">
+            <q-btn class="q-mt-xl" style="margin-right: 10px;" color="primary" label="Register" type="submit" @click="validateAndSubmit" />
+            <q-btn class="q-mt-xl" color="dark" label="Back to Login" unelevated to="/" @click="goToLogin" />
+        </div>
+    </div>
 </q-page>
 </template>
 
