638c343e7a6c9b6b9dc220e8335719bd3d26e69c
Elia Albanese <eliaalbanese@icloud.com>
2021-11-22 13:12:25 +0100

    created test user

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 insert into User (user_id, firstname, lastname, email, password)
-values(1, 'Elia', 'Albanese', 'eliaalbanese@icloud.com', 'Test123');
+values(1, 'Elia', 'Albanese', 'eliaalbanese@icloud.com', 'EliaAlbanese');
 insert into User (user_id, firstname, lastname, email, password)
 values(2, 'TEST', 'Testuser', 'TEst@icloud.com', 'Test123');
